80894db920b46f4b9dbefabdffa892d303f9d983
sunnygazi <45737656+sunnygazi@users.noreply.github.com>
2019-02-01 21:40:18 -0500

    nn

---
+++ src/com/tt/classes/basicJava.java
@@ -0,0 +1,18 @@
+package com.tt.classes;
+
+public class basicJava {
+
+	public static void main(String[] args) {
+//If else 
+		int x = 34;
+		if (x < 21)
+			System.out.println("too small");
+
+		else if (x > 20)
+
+			System.out.println("too big");
+		else
+			System.out.println("does not apply");
+	}
+
+}
